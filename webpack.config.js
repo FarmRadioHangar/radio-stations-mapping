bffb946c30d06807c1911704bfd58833f95d3a69
Johannes Hilden <johannes@isomorphic.co>
2016-08-05 10:07:35 +0300

    set output path to public/ in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: './main.js',
-  output: { path: __dirname, filename: 'bundle.js' },
+  output: { path: path.join(__dirname, 'public'), filename: 'bundle.js' },
   module: {
     loaders: [
       {
